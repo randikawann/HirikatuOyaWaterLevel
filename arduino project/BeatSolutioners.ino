d08eb79fa559c5ae016f2d0e038e98a2aa68ab9b
Randika wanninayaka <randikawann@gmail.com>
2020-04-12 08:35:20 +0530

    added arduino project

---
+++ arduino project/BeatSolutioners.ino
@@ -0,0 +1,114 @@
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(9, 10);
+
+void setup() {
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  Serial.begin(9600);
+}
+int waterLevel;
+
+void loop() {
+  
+  waterLevel=analogRead(A0);
+  
+  Serial.print(" ");
+  
+  waterLevel=map(waterLevel,0,700,0,12);
+  Serial.println(waterLevel);
+
+ if(waterLevel<=4){
+    setColor(0,255,0);
+    digitalWrite(11,LOW );//green
+  }
+
+  if(waterLevel>4 && waterLevel<8){
+    setColor(255,50,0);
+    digitalWrite(11,LOW);//yellow
+  }
+
+  if(waterLevel>=8){
+    digitalWrite(11,HIGH);
+    setColor(255,0,0);//red
+    waterLevel=waterLevel;
+  }
+  
+ /* if(waterLevel<0){
+    waterLevel=0;
+  } 
+    if (waterLevel>2)
+
+    {
+
+      SendMessage();
+
+
+    }
+
+
+    if (waterLevel>4)
+
+  {
+
+      DialCall();
+
+  }
+}
+
+ void SendMessage(){
+
+  mySerial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+
+  delay(1000);  // Delay of 1000 milli seconds or 1 second
+
+  mySerial.println("AT+CMGS=\"+94715943978\"\r"); // Replace x with mobile number
+
+  delay(1000);
+
+  mySerial.println("Duwapalla..gan watura galano...");// The SMS text you want to send
+
+  delay(100);
+
+  mySerial.println((char)26);// ASCII code of CTRL+Z
+
+  delay(1000);
+
+}
+
+ /*void RecieveMessage()
+
+{
+
+  mySerial.println("AT+CNMI=2,2,0,0,0"); // AT Command to recieve a live SMS
+
+  delay(1000);
+
+}
+
+*/
+/*
+  void DialCall()
+
+ {
+
+  mySerial.println("ATD+94715943978;"); // ATDxxxxxxxxxx; -- watch out here for semicolon at the end!!
+
+  delay(100);
+
+ }
+ */
+
+}
+
+void setColor(int redValue,int greenValue,int blueValue){
+    analogWrite(8, redValue);
+    analogWrite(9, greenValue);
+    analogWrite(10, blueValue);
+    
+  }
+
+  
---
+++ arduino project/WaterLevel.ino
@@ -0,0 +1,47 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+void setup() {
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  Serial.begin(9600);
+}
+int waterLevel;
+
+void loop() {
+  lcd.setCursor(0,0);
+  
+  waterLevel=analogRead(A0);
+  Serial.println(waterLevel);
+  //Serial.print(" ");
+  
+  waterLevel=map(waterLevel,470,630,0,5);
+  if(waterLevel<0){
+    waterLevel=0;
+  }
+  //Serial.println(waterLevel);
+  lcd.print("Water Level=");
+  lcd.print(waterLevel);
+  lcd.print("cm");
+  delay(100);
+  lcd.clear();
+
+  lcd.setCursor(4,2);
+  lcd.print("CIS SUSL");
+  /*for(int i=0;i<11;i++){
+    lcd.setCursor(i,0);
+    lcd.print("Nayana");
+    delay(200);
+    lcd.clear();
+  }
+  for(int i=11;i>0;i--){
+    lcd.setCursor(i,0);
+    lcd.print("Nayana");
+    delay(200);
+    lcd.clear();
+  }*/
+}
+
+
+
